a13b998e92d7f1c11d06f1ee8ca1dc8148be8d7a
Matthias J. Kannwischer <matthias@kannwischer.eu>
2024-06-19 18:04:28 +0800

    change

--- dummy.py
+++ dummy.py
@@ -2,4 +2,23 @@
 import random
 
 
-print(random.randint(0,10000))
+
+v = [
+    {
+        "name": "keygen",
+        "unit": "cycles",
+        "value": random.randint(0,10000)
+    },
+    {
+        "name": "encaps",
+        "unit": "cycles",
+        "value": random.randint(0,10000)
+    },
+    {
+        "name": "decaps",
+        "unit": "cycles",
+        "value": random.randint(0,10000)
+    },
+]
+
+print(v)
