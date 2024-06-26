c9049bf363c38d8c4dba11a25ea7122d581f5219
github-action-benchmark <github@users.noreply.github.com>
2024-06-26 05:37:32 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for aa48e53bdadd81f338ec854d96434f65024effb5

---
+++ dev/bench/data.js
@@ -0,0 +1,43 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1719380252087,
+  "repoUrl": "https://github.com/mkannwischer/bench-test",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "mkannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "name": "mkannwischer",
+            "username": "mkannwischer"
+          },
+          "id": "aa48e53bdadd81f338ec854d96434f65024effb5",
+          "message": "Update README",
+          "timestamp": "2024-06-26T05:36:32Z",
+          "url": "https://github.com/mkannwischer/bench-test/pull/3/commits/aa48e53bdadd81f338ec854d96434f65024effb5"
+        },
+        "date": 1719380251797,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "keygen",
+            "value": 4447,
+            "unit": "cycles"
+          },
+          {
+            "name": "encaps",
+            "value": 6763,
+            "unit": "cycles"
+          },
+          {
+            "name": "decaps",
+            "value": 3778,
+            "unit": "cycles"
+          }
+        ]
+      }
+    ]
+  }
+}
