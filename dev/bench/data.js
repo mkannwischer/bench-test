0247015e355b8d4773a72d8a009a187f6efcd3e9
github-action-benchmark <github@users.noreply.github.com>
2024-06-26 05:54:27 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 3cb07d30e4edaf0297c995170b3824987914ad5f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1719380252087,
+  "lastUpdate": 1719381267825,
   "repoUrl": "https://github.com/mkannwischer/bench-test",
   "entries": {
     "Benchmark": [
@@ -34,6 +34,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "decaps",
             "value": 3778,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3cb07d30e4edaf0297c995170b3824987914ad5f",
+          "message": "Update README\n\nSigned-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>",
+          "timestamp": "2024-06-26T13:54:14+08:00",
+          "tree_id": "15b0c154589dbb49e5382c49e92638c136dd8a6b",
+          "url": "https://github.com/mkannwischer/bench-test/commit/3cb07d30e4edaf0297c995170b3824987914ad5f"
+        },
+        "date": 1719381266960,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "keygen",
+            "value": 1088,
+            "unit": "cycles"
+          },
+          {
+            "name": "encaps",
+            "value": 1773,
+            "unit": "cycles"
+          },
+          {
+            "name": "decaps",
+            "value": 4168,
             "unit": "cycles"
           }
         ]
